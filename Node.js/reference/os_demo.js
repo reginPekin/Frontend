027a64bee1c871351355444e75fbeb1e991115f4
reginPekin <reginbegin@gmail.com>
2019-07-22 11:52:44 +0300

    os code

---
+++ Node.js/reference/os_demo.js
@@ -0,0 +1,13 @@
+const os = require("os");
+
+// Free memory
+console.log(os.freemem());
+
+// Total memory
+console.log(os.totalmem());
+
+// Home dir
+console.log(os.homedir());
+
+// Uptime
+console.log(os.uptime());
